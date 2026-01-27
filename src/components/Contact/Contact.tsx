b4f4878f68ce7a916834c846d5117ed44d7f33eb
Priyanshu Pandey <priyanshupandey@Priyanshus-MacBook-Air-3.local>
2026-01-28 01:23:34 +0530

    Initial commit

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,28 @@
+import { Container } from "./styles";
+import emailIcon from "../../assets/email-icon.svg";
+import phoneIcon from "../../assets/phone-icon.svg"
+
+
+
+export function Contact() {
+
+  return (
+    <Container id="contact">
+      <header>
+        <h2>Contact</h2>
+        <p>Ready to get started on your project? </p>
+        <p>Contact me now for a Free consultation.</p>
+      </header>
+      <div className="contacts">
+        <div>
+          <a href="mailto:priyanshupandey652005@gmail.com"><img src={emailIcon} alt="Email" /></a>
+          <a href="mailto:priyanshupandey652005@gmail.com">priyanshupandey652005@gmail.com</a>
+        </div>
+        <div>
+          <a href="tel:+919721213185"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+919721213185">(+91) 9721213185</a>
+        </div>
+      </div>
+    </Container>
+  )
+}
