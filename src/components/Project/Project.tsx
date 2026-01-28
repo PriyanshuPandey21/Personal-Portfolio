61f9b4bed7afe454923b908017add51f9dc2fd8f
Priyanshu Pandey <priyanshupandey@Priyanshus-MacBook-Air-3.local>
2026-01-28 13:59:03 +0530

    Update Connectify project description

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -45,7 +45,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-                <a href="https://github.com/PriyanshuPandey/Serverless-Voting-Application" target="_blank" rel="noreferrer">
+                <a href="https://github.com/PriyanshuPandey21/Connectify-Seamless-video-calling" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" /></a>
                 {/* <a href="https://vote.priyanshupandey.com" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
@@ -53,10 +53,10 @@ export function Project() {
               </div>
             </header>
             <div className="body">
-              <h3>Serverless Voting Application for Programming Languages</h3>
-              <p> Developed a serverless voting app using React, AWS Lambda, API Gateway, and DynamoDB, enabling users to view programming languages, cast votes, and access details through an interactive UI, leveraging serverless architecture for scalability and cost-efficiency. </p>
+              <h3>Connectify-Seamless-video-calling-website</h3>
+              <p> Connectify is a real-time video communication web application built using React.js for a component-driven UI architecture and Tailwind CSS for utility-first, responsive styling. The frontend leverages React Hooks, Context API, and state-driven rendering to deliver a performant and highly interactive user experience</p>
             </div>
-            <footer> <ul className="tech-list"> <li>AWS Lambda</li> <li>API Gateway</li> <li>DynamoDB</li> </ul> </footer>
+            <footer> <ul className="tech-list"> <li>React</li> <li>Tailwind CSS</li> <li>Context API</li> </ul> </footer>
           </div>
         </ScrollAnimation>
 
